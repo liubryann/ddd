4f34716b7a88184dd5df5defe190005c6c3c269a
tt-le <tienthanh.le@mail.utoronto.ca>
2020-11-21 01:57:54 -0500

    moved db to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT = 4000;
 
+const ADMIN = process.env.MONGO_ADMIN;
+const PW = process.env.MONGO_PW;
+const DBNAME = process.env.DBNAME;
+const uri = `mongodb+srv://${ADMIN}:${PW}@cluster0.7lsr0.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Database connection successful");
+});
+
+const dataSchema = mongoose.Schema({
+    sector: { 
+        type: String,
+        required: true
+    },
+    symbol: {
+        type: String,
+        required: false
+    },
+    date: String,
+    sentiment: String,
+    summary: String
+})
+
+const Data = mongoose.model("Data", dataSchema)
+
+Data.find((err, result) => {
+    if (err) return console.error(err);
+    console.log(result);
+    return result
+})
+
+module.exports = {db, Data}
 app.use(cors());
 
 app.listen(PORT, function() {
