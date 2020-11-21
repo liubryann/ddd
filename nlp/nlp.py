68f74a70a8a2402f034d814cac8e54342cb12c40
tt-le <tienthanh.le@mail.utoronto.ca>
2020-11-21 17:07:20 -0500

    gcp category analysis

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -2,6 +2,9 @@ from google.cloud import language_v1
 
 # pip install --upgrade google-cloud-language
 
+document = {"type_": language_v1.Document.Type.PLAIN_TEXT, "language": "en"}
+encoding_type = language_v1.EncodingType.UTF8
+
 def analyze_sentiment(text_content):
     """
     Analyzing Sentiment in a String
@@ -11,12 +14,7 @@ def analyze_sentiment(text_content):
     """
 
     client = language_v1.LanguageServiceClient()
-
-    type_ = language_v1.Document.Type.PLAIN_TEXT
-    language = "en"
-    document = {"content": text_content, "type_": type_, "language": language}
-    encoding_type = language_v1.EncodingType.UTF8
-
+    document["content"] = text_content
     response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
 
     print("Document sentiment score: {}".format(response.document_sentiment.score))
@@ -32,3 +30,19 @@ def analyze_sentiment(text_content):
         print("Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
 
     print("Language of the text: {}".format(response.language))
+
+def classify_text(text_content):
+    """
+    Classifying Content in a String
+
+    Args:
+      text_content The text content to analyze. Must include at least 20 words.
+    """
+
+    client = language_v1.LanguageServiceClient()
+    document["content"] = text_content
+    response = client.classify_text(request = {'document': document})
+    for category in response.categories:
+        print("Category name: {}".format(category.name))
+        print("Confidence: {}".format(category.confidence))
+
