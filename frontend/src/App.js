846e0ba381175546754453ab9075bf3b0c31f01a
Eric <erickh.li@mail.utoronto.ca>
2020-11-21 20:56:01 -0500

    DDD-15 charting no luck

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './ddd.svg';
 import './App.css';
+import Chart from './components/Chart';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} />
       Due Diligence for Dummies!
+      <Chart/>
     </div>
   );
 }
